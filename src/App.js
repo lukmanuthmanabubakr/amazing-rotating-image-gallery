4510b68a4774881f7ebd41163f7b9897e6d88fd9
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-20 23:23:29 +0100

    add new favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import RotatingImageGallery from './RotatingImageGallery/RotatingImageGallery';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <RotatingImageGallery />
     </div>
   );
 }
